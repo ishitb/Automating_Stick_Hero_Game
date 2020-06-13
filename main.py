860c83ff6603770fab866afbd581d4b4d2c9b189
ishitb <ib370@snu.edu.in>
2020-06-13 22:05:47 +0530

    complete

--- main.py
+++ main.py
@@ -1,20 +1,45 @@
+import os
 import numpy
 from PIL import Image
+import time
 
-image = Image.open('screen.png')
-image = numpy.array(image, dtype=numpy.uint8)
+while True :
+    os.system('adb shell screencap /sdcard/screen.png')
+    os.system('adb pull /sdcard/screen.png')
 
-pixels = [list(i[:3]) for i in image[1700]]
+    image = Image.open('screen.png')
+    image = numpy.array(image, dtype=numpy.uint8)
 
-area = []
-ignore = True
+    pixels = [list(i[:3]) for i in image[1700]]
 
-for i, pixel in enumerate(pixels) :
-    r, g, b = [int(i) for i in pixel]
+    area = []
+    ignore = True
+    black= True
+
+    for i, pixel in enumerate(pixels) :
+        r, g, b = [int(i) for i in pixel]
+        
+        if ignore and (r + g + b) != 0 :
+            continue
+
+        ignore = False
+
+        if black and (r + g + b) != 0 :
+            black = not black
+            area.append(i)
+            continue
+
+        if not black and (r + g + b) == 0 :
+            black = not black
+            area.append(i)
+            continue
+
+    start, targetS, targetE = area
+
+    whitespace = targetS - start
+    targetWidth = targetE - targetS 
+    longPressTime = whitespace + targetWidth/2
+
+    os.system(f'adb shell input swipe 540 540 540 540 {int(longPressTime)}')
     
-    if ignore and (r + g + b) != 0 :
-        continue
-
-    ignore = False
-    print(r, g, b)
-    break
+    time.sleep(2.5)
